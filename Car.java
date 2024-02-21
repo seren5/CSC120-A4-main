ad28eefa072883136cd6fd0edd573cc657c56a21
Emily Ren <shuyueeren@gmail.com>
2024-02-20 19:31:59 -0500

    comments/javadocs on engine

--- Car.java
+++ Car.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class Car {
+
+    ArrayList<String> passengersOnBoard = new ArrayList<>();
+    int maxCapacity;
+
+    public Car() {}
 
 }
--- Engine.java
+++ Engine.java
@@ -1,20 +1,24 @@
 public class Engine {
 
+    /** Attributes */
     FuelType fuelType;
     double currentFuel;
     double maxFuel;
 
+    /** Constructor that takes in initial values and sets them appropriately */
     public Engine(FuelType fuelType, double currentFuel, double maxFuel) {
         this.fuelType = fuelType;
         this.currentFuel = currentFuel;
         this.maxFuel = maxFuel;
     }
 
+    /** Refuel method to reset current fuel level to maximum */
     public void refuel() {
         currentFuel = maxFuel;
     }
 
-    public go() {
+    /** Go method that decreases the fuel level, prints remaining fuel level, and returns true or false */
+    public boolean go() {
         if (currentFuel > 0){
             currentFuel -= 1;
             System.out.println("The remaining fuel level is ", currentFuel);
@@ -24,6 +28,7 @@ public class Engine {
             return false;
         }
     }
+
     public static void main(String[] args) {
         Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
         while (myEngine.go()) {
