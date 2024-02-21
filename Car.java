eacb28aaa6d7530aa1b8c7f1669609658c43df96
Emily Ren <shuyueeren@gmail.com>
2024-02-20 20:29:15 -0500

    engine fixed

--- Car.java
+++ Car.java
@@ -2,9 +2,46 @@ import java.util.ArrayList;
 
 public class Car {
 
-    ArrayList<String> passengersOnBoard = new ArrayList<>();
-    int maxCapacity;
+    ArrayList<String> passengersOnBoard = new ArrayList<>(); // Stores the passengers currently on board
+    int maxCapacity; // Maximum capacity of the car
 
-    public Car() {}
+    /** Constructor that takes in initial value for maximum capacity and initializes Array list */
+    public Car() {
+        this.maxCapacity = maxCapacity;
+    }
+
+    /** Accessor-like method that returns the maximum capacity */
+    public int getCapacity() {
+        return maxCapacity;
+    }
+
+    /** Accessor-like method that returns the remaining seats */
+    public int seatsRemaining() {
+        return (maxCapacity - passengersOnBoard.length);
+    }
+
+    /** Adds a passenger */
+    private addPassenger(Passenger p) {
+        if (seatsRemaining > 0) {
+
+        }
+    }
+    
+    /** Removes a passenger */
+    private removePassenger(Passenger p) {
+        if ((Passenger p) in Car) {
+
+        }
+    }
+
+    /** Final method that prints out a list of all Passengers aboard the car */
+    public printManifest() {
+        if (/** number of passengers on car */ > 0) {
+            for (int i=0; i < /** number of elements in array */; i++){
+                System.out.println(passengersOnBoard.get(i));
+        }
+    }   else{
+            System.out.println("This car is EMPTY.");
+    }
 
 }
--- Engine.java
+++ Engine.java
@@ -6,10 +6,9 @@ public class Engine {
     double maxFuel;
 
     /** Constructor that takes in initial values and sets them appropriately */
-    public Engine(FuelType fuelType, double currentFuel, double maxFuel) {
+    public Engine(FuelType fuelType, double currentFuel) {
         this.fuelType = fuelType;
         this.currentFuel = currentFuel;
-        this.maxFuel = maxFuel;
     }
 
     /** Refuel method to reset current fuel level to maximum */
@@ -21,7 +20,7 @@ public class Engine {
     public boolean go() {
         if (currentFuel > 0){
             currentFuel -= 1;
-            System.out.println("The remaining fuel level is ", currentFuel);
+            System.out.println("The remaining fuel level is " + currentFuel);
             return true;
         }
         else{
