061c196c381a22fc4eba1750b3426521ab154454
Emily Ren <shuyueeren@gmail.com>
2024-02-21 22:50:50 -0500

    docs

--- Train.java
+++ Train.java
@@ -19,18 +19,35 @@ public class Train {
         this.passengerCapacity = passengerCapacity;
     }
 
+    /** Accessor to get engine */
     public Engine getEngine() {
         return engine;
     }
 
+    /** Accessor to return the "i"th car */
     public Car getCar(int i) {
         return carAttached.get(i);
     }
 
+    /** Accessor to return the maximum total capacity across all cars */
+    public int getMaxCapacity() {
+        int maxCap = 0;
+        for (Car car:this.carAttached){
+            maxCap = maxCap + car.getCapacity();
+        }
+        return maxCap;
+    }
+
+    /** Accessor to return the number of remaining open seats */
     public int seatsRemaining() {
+        int seatsLeft = 0;
+        for (Car car:this.carAttached){
+            seatsLeft = seatsLeft + car.seatsRemaining();
+        }
         return seatsRemaining();
     }
 
+    /** Prints a roster of all passengers on board */
     public void printManifest() {
         for (Car car:this.carAttached) {car.printManifest();}
     }
